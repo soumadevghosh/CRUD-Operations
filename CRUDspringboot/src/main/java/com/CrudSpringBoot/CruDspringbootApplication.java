1abe02df3beeb173e380d267371163b7487a3d54
Soumadev <soumadevghosh@gmail.com>
2020-04-28 22:38:26 +0530

    v1

---
+++ CRUDspringboot/src/main/java/com/CrudSpringBoot/CruDspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.CrudSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDspringbootApplication.class, args);
+	}
+
+}
